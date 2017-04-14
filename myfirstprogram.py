a48ac5acf429296cf0a2d7010829233d18e0a8d9
kayib <ggkhalid@outlook.com>
2017-04-14 01:04:43 +0100

    Add files via upload

---
+++ myfirstprogram.py
@@ -0,0 +1,83 @@
+kay = int(input('put a number'))
+
+if kay < 0:
+    print('negative number')
+
+elif kay in range(0,10):
+    print ('units')
+
+elif kay in range(10,100):
+    print ('tenth')
+
+elif kay in range(100,1000):
+    print ('hunderethn')
+else:
+    print ('End')
+
+
+kay = int(input('put a number'))
+
+if kay < 0:
+    print('negative number')
+
+elif kay in range(0,10):
+    print ('units')
+
+elif kay in range(10,100):
+    print ('tenth')
+
+elif kay in range(100,1000):
+    print ('hunderethn')
+else:
+    print ('End')
+
+
+kay = int(input('put a number'))
+
+if kay < 0:
+    print('negative number')
+
+elif kay in range(0,10):
+    print ('units')
+
+elif kay in range(10,100):
+    print ('tenth')
+
+elif kay in range(100,1000):
+    print ('hunderethn')
+else:
+    print ('End')
+
+
+kay = int(input('put a number'))
+
+if kay < 0:
+    print('negative number')
+
+elif kay in range(0,10):
+    print ('units')
+
+elif kay in range(10,100):
+    print ('tenth')
+
+elif kay in range(100,1000):
+    print ('hunderethn')
+else:
+    print ('End')
+
+kay = int(input('put a number'))
+
+if kay < 0:
+    print('negative number')
+
+elif kay in range(0,10):
+    print ('units')
+
+elif kay in range(10,100):
+    print ('tenth')
+
+elif kay in range(100,1000):
+    print ('hunderethn')
+else:
+    print ('End')
+  
